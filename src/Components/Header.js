1393bf537f92972fb3ba263f2ecf53ed1de2253c
Vishal Goswami <vishalgoswami2250@gmail.com>
2024-05-30 13:55:49 +0530

    search field

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,34 @@
-import React from "react";
+'use client'
+import React,{useState} from "react";
 import Image from "next/image";
 import styles from './Header.module.css'; 
 
 const Header = () => {
+    const [showSearch,setShowSearch] = useState(false)
+    const handleSearchClick = () => {
+        setShowSearch(!showSearch);
+    }
     return (
         <div>
             <div className={styles.headerContainer}>
                 <h2 className={styles.title}>T A N N  T R I M</h2>
                 <div className={styles.iconContainer}>
-                    <Image className={styles.icon} src={require("../assets/search.png")} alt="Search" />
+                {showSearch ? (
+                        <input
+                            type="text"
+                            className={styles.searchInput}
+                            onBlur={() => setShowSearch(false)}
+                            placeholder="Search..."
+                        />
+                    ) : (
+                        <Image
+                            className={styles.icon}
+                            src={require("../assets/search.png")}
+                            alt="Search"
+                            onClick={handleSearchClick}
+                        />
+                    )}
+                    {/* <Image className={styles.icon} src={require("../assets/search.png")} alt="Search" /> */}
                     <Image className={styles.icon} src={require("../assets/user-alt.png")} alt="user" />
                     <Image className={styles.icon} src={require("../assets/Frame 54.png")} alt="bag" />
                     <Image className={styles.icon} src={require("../assets/Frame 53.png")} alt="notification" />
